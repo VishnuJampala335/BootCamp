859962069719e624d27e7be201cb86d0b844a5d0
VishnuJampala <69258078+VishnuJampala335@users.noreply.github.com>
2020-08-07 17:24:16 +0530

    Add files via upload

---
+++ URLshortener/urlshort/handler.go
@@ -0,0 +1,89 @@
+package urlshort
+
+import (
+	"encoding/json"
+	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	// 3. use Maphandler
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+// 1. To parse yaml
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+// 2. change yaml or json into map
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.Url
+	}
+	return pathsToUrls
+}
+
+//struct to support parsing of both yaml and json
+type pathUrl struct {
+	Path string `yaml:"path" json:"path"`
+	Url  string `yaml:"url" json :"url"`
+}
+
+func JsonHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+
+	var jsonUrl []pathUrl
+	err := json.Unmarshal(jsonBytes, &jsonUrl)
+	if err != nil {
+		return nil, err
+	}
+	jsonpathUrls := buildMap(jsonUrl)
+
+	return MapHandler(jsonpathUrls, fallback), nil
+}
